76eef11b0d4ee4b1f96f202aed0977eb0e2d5bcb
nayarawatanuki <nayarawatanuki@gmail.com>
2023-03-28 17:12:34 -0300

    optimizing functions with `for()`

--- js/index.js
+++ js/index.js
@@ -1,27 +1,20 @@
 var $ = document.querySelector.bind(document);
 
 const keyList = document.querySelectorAll(".key");
-let counter = 0;
 
 function playSound(audioId) {
 
     $(audioId).play();
 }
 
-while(counter < keyList.length) {
+for(counter = 0; counter < keyList.length; counter++) {
 
     const key = keyList[counter];
     const instrument = key.classList[1];
-
-    // console.log(instrument);
-
     const audio__id = `#sound__${instrument}`;
 
     key.onclick = function() {
         playSound(audio__id);
     }
-
-    counter++;
-
-    // console.log(counter);
+    
 }
